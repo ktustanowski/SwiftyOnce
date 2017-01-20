9104e3fc60f1f94f16240c18d4ab9418f01b013a
ktustanowski <kamil.tustanowski@gmail.com>
2017-01-20 12:04:03 +0100

    Initial Commit

---
+++ SwiftyOnce/SwiftyOnce.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyOnce.h
+//  SwiftyOnce
+//
+//  Created by Kamil Tustanowski on 20.01.2017.
+//  Copyright © 2017 ktustanowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyOnce.
+FOUNDATION_EXPORT double SwiftyOnceVersionNumber;
+
+//! Project version string for SwiftyOnce.
+FOUNDATION_EXPORT const unsigned char SwiftyOnceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyOnce/PublicHeader.h>
+
+
