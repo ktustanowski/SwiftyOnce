44cca9aedfd5570afc0e56583001d863c883c248
Steve Jun <juns@Steves-MacBook-Pro.local>
2018-10-29 14:56:17 -0400

    Add tvOS framework

---
+++ SwiftyOnceTV/SwiftyOnceTV.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyOnceTV.h
+//  SwiftyOnceTV
+//
+//  Created by Steve Jun on 10/29/18.
+//  Copyright © 2018 ktustanowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyOnceTV.
+FOUNDATION_EXPORT double SwiftyOnceTVVersionNumber;
+
+//! Project version string for SwiftyOnceTV.
+FOUNDATION_EXPORT const unsigned char SwiftyOnceTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyOnceTV/PublicHeader.h>
+
+
